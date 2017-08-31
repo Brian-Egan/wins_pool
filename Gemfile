5618944182c98bdfd8eef1c4f278b0302df6ffed
Brian Egan <brian_egan@discovery.com>
2017-08-31 12:51:34 -0400

    updated gemfile for rails 5 and tightened the logic in the teams model

--- Gemfile
+++ Gemfile
@@ -1,31 +1,50 @@
 source 'https://rubygems.org'
 # ruby '2.1.2'
-gem 'rails', '4.1.6'
-gem 'sass-rails', '~> 4.0.3'
-gem 'uglifier', '>= 1.3.0'
-gem 'coffee-rails', '~> 4.0.0'
-gem 'jquery-rails'
-gem 'turbolinks'
-gem 'jbuilder', '~> 2.0'
+
+# gem 'rails', '4.1.6'
+# gem 'sass-rails', '~> 4.0.3'
+# gem 'uglifier', '>= 1.3.0'
+# gem 'coffee-rails', '~> 4.0.0'
+# gem 'jquery-rails'
+# gem 'turbolinks'
+# gem 'jbuilder', '~> 2.0'
+
+# First attempt at update
+# gem "rails"
+# gem "json"
+gem "sass-rails"
+gem "uglifier"
+gem "coffee-rails"
+gem "jquery-rails"
+gem "turbolinks"
+gem "jbuilder"
+
+
+gem 'rails', '5.1.3'
+
 gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 gem 'bootstrap-sass'
-gem 'haml-rails'
+gem 'haml'
 gem 'high_voltage'
 gem 'puma'
+
+  gem 'sqlite3'
 group :development do
   gem 'better_errors'
   gem 'binding_of_caller', :platforms=>[:mri_21]
   gem 'html2haml'
   gem 'hub', :require=>nil
-  gem 'quiet_assets'
+  # gem 'quiet_assets'
   gem 'rails_layout'
+  gem "annotate"
+  gem "haml-rails", "~> 1.0"
 end
 group :development, :test do
-  gem 'sqlite3'
+  # gem 'sqlite3'
 end
 group :production do
-  gem 'pg'
+  # gem 'pg'
   gem 'rails_12factor'
 
 end
@@ -39,4 +58,5 @@ gem "nokogiri"
 gem "awesome_print"
 
 
-ruby "2.2.0"
+# ruby "2.2.0"
+# 
--- app/models/team.rb
+++ app/models/team.rb
@@ -13,17 +13,54 @@ class Team < ActiveRecord::Base
     # standings = Array.new
     # t = Team.create(name: "The test worked")
     rows = doc.xpath("//table/tbody/tr")
+    header_row = rows.select{|x| x.text.include?("AFC")}.first
+    headers = hr.css("td").map{|x| x.inner_text.strip}
+    headers[0] = "Team" # Instead of being set to "AFC East Team"
+    rows = rows.reject{|x| (x.text.include?("AFC") or x.text.include?("NFC") or x.text.include?("Conference") or x.text.empty?)}
     rows.each do |row|
-        unless (row.text.include?("AFC") or row.text.include?("NFC") or row.text.include?("Conference") or row.text.empty?)
-            record = row.css("td").map{|x| x.text.strip}[0..3]
-            team = self.find_or_create_by(name: record[0])
-            team.update_attributes(wins: record[1], losses: record[2], ties: record[3])
-            # team.save if team.changed?
-            team.save
-        end
+        # unless (row.text.include?("AFC") or row.text.include?("NFC") or row.text.include?("Conference") or row.text.empty?)
+        team_hsh = {}
+        headers.each_with_index{|h, i| team[h] = row[i]}
+        team = self.find_or_create_by(name: team_hsh["Team"])
+        team.update_attributes(wins: team_hsh["W"].to_i, losses: team_hsh["L"].to_i, ties: team_hsh["T"].to_i)
+        team.save
+
+        ## Below is old way from 2015
+        # unless (row.text.include?("AFC") or row.text.include?("NFC") or row.text.include?("Conference") or row.text.empty?)
+        # record = row.css("td").map{|x| x.text.strip}[0..3]
+        # team = self.find_or_create_by(name: record[0])
+        # team.update_attributes(wins: record[1], losses: record[2], ties: record[3])
+        # # team.save if team.changed?
+        # team.save
+        # end
     end
 
   end
+
+
+  # The team_hsh above gives us more info on teams that we can use if wanted:
+    # {
+    #   "Team" => "New England Patriots",
+    #     "W" => "0",
+    #     "L" => "0",
+    #     "T" => "0",
+    #   "Pct" => ".000",
+    #    "PF" => "0",
+    #    "PA" => "0",
+    # "Net Pts" => "0",
+    #    "TD" => "0",
+    #  "Home" => "0-0",
+    #  "Road" => "0-0",
+    #   "Div" => "0-0",
+    #  "Conf" => "0-0",
+    # "Non-Conf" => "0-0",
+    # "Streak" => "--",
+    # "Last 5" => "0-0"
+    # }
+
+    # We should definitely add PF and PA as a tie-breaker between teams with the same amount of wins.
+
+  # (x.text.include?("AFC") or x.text.include?("NFC") or x.text.include?("Conference") or x.text.empty?)
 
 
 
